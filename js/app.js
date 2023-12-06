d08b7c0b04665b37e13a27adf70f66b3d6756850
Travis Schlief <travisschlief@hotmail.com>
2023-12-05 22:37:44 -0800

    fix mobile css

--- js/app.js
+++ js/app.js
@@ -1,27 +1,18 @@
 
 
-    const createBtn = document.getElementById("create-btn");
     const editBtn = document.getElementById("edit-btn");
-    const submitBtn = document.getElementById("submit-btn");
     const playBtn = document.getElementById("play-btn");
     var maze = new Maze();
    
 
-    const handleCreateBtn =(event) => {
-        maze.checkFog(44);
-        console.log("clicked create");
-    }
+ 
     
     const handleEditBtn =(event) => {
         
         maze.enableEditMode();
         console.log("clicked edit");
     }
-    
-    const handleSubmitBtn =(event) => {
-        console.log("clicked submit");
-    }
-    
+   
     const handlePlayBtn =(event) => {
         console.log("clicked play");
 
@@ -32,9 +23,7 @@
 
 
 
-    document.getElementById("create-btn").addEventListener("click", handleCreateBtn);
     document.getElementById("edit-btn").addEventListener("click", handleEditBtn);
-    document.getElementById("submit-btn").addEventListener("click", handleSubmitBtn);
     document.getElementById("play-btn").addEventListener("click", handlePlayBtn);
 
 
