2bc3f512dd63ea082c8864eeff8e1b77dc29c05c
Travis Schlief <travisschlief@hotmail.com>
2023-12-07 19:44:20 -0800

    fix finished message bug

--- js/victory.js
+++ js/victory.js
@@ -1,17 +1,24 @@
 class Victory{
     constructor (victoryElement, elementToOverlay) {
         this.banner = document.getElementById(victoryElement);
+        this.banner.style.cssText= "display: none";
+        this.banner.innerHTML = "Finished";
         this.target = document.getElementById(elementToOverlay);
 
         window.addEventListener("resize", ()=>this.updatePosition());
         this.updatePosition();
         
-        this.banner.style.cssText= "display: none;";
+        
 
     }
 
     updatePosition(){
-       
+        console.log("banner false", this.banner.style.display)
+        if(!this.banner.style.display || this.banner.style.display !== "flex"){
+            return;
+        }
+        
+        
             const bannerRect = this.banner.getBoundingClientRect();
             const targetRect = this.target.getBoundingClientRect();
             const top = targetRect.top + (targetRect.height/2) - (bannerRect.height);
@@ -20,25 +27,25 @@ class Victory{
 
 
             this.banner.style.cssText = `
+                display: flex;
                 top: ${top}px;
                 left: ${left}px;
                 width: ${width}px;
             
             `
-            console.log(targetRect);
+         
        
     }
 
 
 
     close(){
-        this.banner.style.cssText="display: none;";
+        this.banner.style.cssText = "display: none";    
     }
 
 
     open(){
-        this.banner.style.cssText="display: flex;"
+        this.banner.style.cssText= "display: flex"
         this.updatePosition();
-        console.log("open")
     }
 }
